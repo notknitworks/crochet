64b4a44d0007e09058243eb42e27dc963053ef7b
npatki <nehapatki@gmail.com>
2013-01-18 20:42:52 -0500

    more login stuff

--- crochetDev/urls.py
+++ crochetDev/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout
-from crochetDev.views import homePage, hello, createUser, login
+from crochetDev.views import homePage, hello, createUser
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
