99e6d83ba7b31ad9d20e474705c16828ad47ff6b
npatki <nehapatki@gmail.com>
2013-01-18 19:28:15 -0500

    trying login stuff

--- crochetDev/settings.py
+++ crochetDev/settings.py
@@ -1,4 +1,6 @@
 # Django settings for crochetDev project.
+import os.path
+PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -106,6 +108,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, "templates"),
 )
 
 INSTALLED_APPS = (
--- crochetDev/views.py
+++ crochetDev/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
-from django.template import Context
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.models import User
 
 def homePage(request):
-	return HttpResponse("Welcome to crochet!")
+	if request.method == 'POST':
+		#form = UserCreationForm(request.POST)
+		useName = request.POST['username']
+		passWord = request.POST['password']
+		user = User.objects.create_user(username=useName,password=passWord)
+		user.save()
+		return HttpResponseRedirect("/")
+	return render_to_response("homepage.html", {}, context_instance=RequestContext(request))
 
 def hello(request):
 	return HttpResponse("Hello")
